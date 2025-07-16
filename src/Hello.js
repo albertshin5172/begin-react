31a9bfe94a921253e8711f7339c341e90cf8cdc9
albert <alertshin5172@gmail.com>
2025-07-16 20:20:52 +0900

    Git Commit Test

--- src/Hello.js
+++ src/Hello.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-function Hello({ color, name = "테스트" }) {
+function Hello({ color, name = "TEST" }) {
   return <div style={{ color }}>안녕하세요 {name}</div>
 }
 
 Hello.defaultProps = {
-  name: '이름없음'
+  name: 'COMMIT TEST'
 }
 
 export default Hello;
